6031e30e8c99bc1c3dd4d8246bcbcb0acfc47308
Beamer <beamer@trnsfr.com>
2011-06-05 12:52:18 -0700

    Initial website

---
+++ javascripts/application.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function() {
+
+  $(".video").click(function() {
+  	$.fancybox({
+  			'padding'		: 0,
+  			'autoScale'		: false,
+  			'transitionIn'	: 'none',
+  			'transitionOut'	: 'none',
+  			'title'			: this.title,
+  			'width'		: 680,
+  			'height'		: 495,
+  			'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+  			'type'			: 'swf',
+  			'swf'			: {
+  			   	 'wmode'		: 'transparent',
+  				'allowfullscreen'	: 'true'
+  			}
+  		});
+  	return false;
+  });
+
+});
